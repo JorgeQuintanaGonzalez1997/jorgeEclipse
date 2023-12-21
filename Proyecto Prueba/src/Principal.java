bb66f77d1f25d7b969d9c82f96b8fc699b827c2c
JorgeQuintanaGonzalez1997 <127245955+JorgeQuintanaGonzalez1997@users.noreply.github.com>
2023-12-21 08:23:15 +0100

    Una forma ingeniosa de despedirse
    Cia pescao

--- Proyecto Prueba/src/Principal.java
+++ Proyecto Prueba/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Holi Wankenobi");
 		System.out.println("Agur Yogur");
+		System.out.println("Cia pescao");
 
 	}
 
